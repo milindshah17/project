a24c400243e0c4bca865c99a670c81a81cc13955
Milind Shah <shahmilind07@gmail.com>
2021-10-21 22:10:12 +0530

    21-10

--- src/app/mycomponents/addproduct/addproduct.component.ts
+++ src/app/mycomponents/addproduct/addproduct.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-addproduct',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddproductComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient) {
+
+    
+  }
 
   ngOnInit(): void {
   }
+
+  onSubmit(data:any)  {
+    let resp = this.http.post("http://localhost:8080/test",data);
+    resp.subscribe((data)=>console.log(data));
+    //let resp2 = this.http.get("http://localhost:8080/event"L);
+    //resp2.subscribe((data)=>this.product=data);
+    console.log(data)
+    
+  }
+
+
+
 
 }
